c83aec1b7be070a026dca3e478a786c14d6f7f84
Danila <leonleonov225@gmail.com>
2022-09-26 21:54:24 +0400

    first commit

---
+++ assets/script/script.js
@@ -0,0 +1,8 @@
+dots = document.querySelectorAll(".dot");
+
+function slideIcon () {
+  let slides = document.querySelectorAll(".slides");
+  for (i = 0; i < slides.length; i++){
+    slides[i].style.displey = 'block';
+  }
+}
