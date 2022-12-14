649288af2459b2b6ff74c2764f730b13141af1e7
moldees <leonleonov225@gmail.com>
2022-12-14 04:28:07 +0400

    finish

--- assets/script/basket.js
+++ assets/script/basket.js
@@ -5,29 +5,65 @@ let pageBasket = document.querySelector('.basket'),
 let allProducts = {
     items : [
         {
-            first_name: "Холодильник",
+            first_name: "Холодильник ",
             second_name: "Samsung",
-            price: "10000"
+            price: "15000",
+            imageSrc: 'assets/image/products/холодильник.jpg',
         },
         {
-            first_name: "Холодильник",
-            second_name: "Samsung",
-            price: "20000",
+            first_name: "Настольная лампа ",
+            second_name: "Alex",
+            price: "1000",
+            imageSrc: 'assets/image/products/настольная лампа.png',
         },
         {
-            first_name: "Холодильник",
-            second_name: "Samsung",
-            price: "30000"
+            first_name: "Настольная лампа ",
+            second_name: "Light",
+            price: "1250",
+            imageSrc: 'assets/image/products/настольная лампа 2.png',
         },
         {
-            first_name: "Холодильник",
-            second_name: "Samsung",
-            price: "40000",
+            first_name: "Стул ",
+            second_name: "Ch",
+            price: "400",
+            imageSrc: 'assets/image/products/стул Ch.jpg',
         },
         {
-            first_name: "Холодильник",
-            second_name: "Samsung",
-            price: "50000",
+            first_name: "Стол ",
+            second_name: "Bakea",
+            price: "3000",
+            imageSrc: 'assets/image/products/Стол Bakea.png',
+        },
+
+        {
+            first_name: "Сковородка ",
+            second_name: "Beko",
+            price: "7899",
+            imageSrc: 'assets/image/products/сковородка bako.jpg',
+        },
+        {
+            first_name: "Сковородка ",
+            second_name: "Stilit",
+            price: "1999",
+            imageSrc: 'assets/image/products/сковородка stilit.png',
+        },
+        {
+            first_name: "Кастрюля ",
+            second_name: "Stilit",
+            price: "6250",
+            imageSrc: 'assets/image/products/Кастрюля Stilit.png',
+        },
+        {
+            first_name: "Дуршлаг ",
+            second_name: "Mako",
+            price: "450",
+            imageSrc: 'assets/image/products/дуршлаг Mako.jpg',
+        },
+        {
+            first_name: "Скалка ",
+            second_name: "Kat",
+            price: "300",
+            imageSrc: 'assets/image/products/Скалка kat.jpg',
         }
     ]
 };
@@ -52,10 +88,6 @@ addEventListener('click', function(event){
         idProduct = target.dataset.id;
 
     if (target && button) {
-        console.log("Название: " + allProducts.items[idProduct].first_name + 
-                                " " + allProducts.items[idProduct].second_name);
-        console.log("Цена: " + allProducts.items[idProduct].price);
-
         basket.itemBasket.push(allProducts.items[idProduct]);
     }
 })
@@ -70,54 +102,104 @@ function closeMainContent(){
         closeProductContent.classList.add('content_info_none');
 }
 
-// function amountPlus(){
-//     let amountProducts = document.querySelector('.basket_menu_amount');
-//     let i = 1;
-//     i++;
-//     amountProducts.textContent = i;
-// }
+function openMainContent(){
+    let closeSlider = document.querySelector('.content_info'),
+        closeTabContent = document.querySelector('.info_tabcontent'),
+        closeProductContent = document.querySelector('.content');
 
-// function amountMinus(){
-//     let amountProducts = document.querySelector('.basket_menu_amount');
-//     let i = 1;
-//     i--;
-//     amountProducts.textContent = i;
-// }
+        closeSlider.classList.remove('content_info_none');
+        closeTabContent.classList.remove('content_info_none');
+        closeProductContent.classList.remove('content_info_none');
+}
 
-// let btnMenuBasket = document.querySelector('input');
+function addProduct(){
+    let basketMenu = document.querySelector('.basket_menu');
+    for(let i = 0; i < basket.itemBasket.length; i++) {
+        let elemDivMenuItem = document.createElement('div'),
+            elemDivNumber = document.createElement('div'),
+            elemDivImage = document.createElement('div'),
+            elemImg = document.createElement('img'),
+            elemDivAmount = document.createElement('div'),
+            elemDivName = document.createElement('div'),
+            elemDivPrice = document.createElement('div'),
+            elemPNumber = document.createElement('p'),
+            elemPName = document.createElement('p'),
+            elemPPrice = document.createElement('p'),
+            elemInputPlus = document.createElement('input'),
+            elemPAmount = document.createElement('p'),
+            elemInputMinus = document.createElement('input'),
+            elemDivDelete = document.createElement('div'),
+            elemInputDelete = document.createElement('input'),
+            elemText_firstName = document.createTextNode(basket.itemBasket[i].first_name),
+            elemText_secondName = document.createTextNode(basket.itemBasket[i].second_name),
+            elemText_price = document.createTextNode(basket.itemBasket[i].price);
 
-// btnMenuBasket.addEventListener('click', function(event) {
-//     let target = event.target;
-//     console.log('done');
-//     if(target && target.classList.contains('change_amount_plus')) {
-//         amountPlus();
-//     }
-//     else if(target && target.classList.contains('change_amount_minus')) {
-//         amountMinus();
-//     }
-// })
+        elemDivMenuItem.classList.add('basket_menu_item');
+        elemDivImage.classList.add('basket_menu_image');
+        elemDivNumber.classList.add('basket_menu_number');
+        elemInputPlus.classList.add('change_amount_plus');
+        elemPAmount.classList.add('basket_menu_amount');
+        elemInputMinus.classList.add('change_amount_minus')
+        elemDivName.classList.add('basket_menu_name');
+        elemDivPrice.classList.add('basket_menu_price');
+        elemDivAmount.classList.add('basket_menu_change_amount')
+        elemDivDelete.classList.add('basket_menu_delete');
+        elemInputDelete.classList.add('delete');
+        
+        elemPName.appendChild(elemText_firstName);
+        elemPName.appendChild(elemText_secondName);
+        elemPPrice.appendChild(elemText_price);
+        elemPNumber.textContent = i+1;
+        elemInputPlus.type = 'button';
+        elemInputPlus.value = '+';
+        elemInputMinus.type = 'button';
+        elemInputMinus.value = '—';
+        elemInputDelete.type = 'button';
+        elemInputDelete.value = '✖';
+        elemPAmount.textContent = '1';
+        elemImg.src = basket.itemBasket[i].imageSrc;
+        elemImg.width = '250';
+        elemImg.height = '150';
+        
+        basketMenu.appendChild(elemDivMenuItem);
+        elemDivMenuItem.appendChild(elemDivNumber);
+        elemDivNumber.appendChild(elemPNumber);
+        elemDivMenuItem.appendChild(elemDivImage);
+        elemDivImage.appendChild(elemImg);
+        elemDivMenuItem.appendChild(elemDivName);
+        elemDivMenuItem.appendChild(elemDivAmount);
+        elemDivMenuItem.appendChild(elemDivPrice)
+        elemDivPrice.appendChild(elemPPrice);
+        elemDivAmount.appendChild(elemInputPlus);
+        elemDivAmount.appendChild(elemPAmount);
+        elemDivAmount.appendChild(elemInputMinus);
+        elemDivName.appendChild(elemPName);
+        elemDivMenuItem.appendChild(elemDivDelete);
+        elemDivDelete.appendChild(elemInputDelete);
+    }
+}
 
-addEventListener('click', function(event){
+let buttonIconBasket = document.querySelector('.btn_basket');
+
+buttonIconBasket.addEventListener('click', function(event){
     let target = event.target;
+
     if(target && target.classList.contains('btn_user_icon') || target.classList.contains('icon basket')
     || target.classList.contains('value_basket')) {
         let basketMenu = document.querySelector('.basket_menu');
+
         basketMenu.style.display = 'block';
         closeMainContent();
-            for(let i = 0; i < basket.itemBasket.length; i++) {
-                let elemDiv = document.createElement('div'),
-                    elemP = document.createElement('p'),
-                    elemImput = document.createElement('input'),
-                    elemText_firstName = document.createTextNode(basket.itemBasket[i].first_name),
-                    elemText_secondName = document.createTextNode(basket.itemBasket[i].second_name),
-                    elemText_price = document.createTextNode(basket.itemBasket[i].price);
-                
-                elemP.appendChild(elemText_firstName);
-                elemP.appendChild(elemText_secondName);
-                elemP.appendChild(elemText_price);
-
-                basketMenu.appendChild(elemDiv);
-                elemDiv.appendChild(elemP);
-            }
+        addProduct();
         }
+
+        let closeBasketMenu = document.querySelector('.close_menu_basket');
+        closeBasketMenu.addEventListener('click', function(event) {
+            if(target && target.classList.contains('btn_user_icon') || target.classList.contains('icon basket')
+            || target.classList.contains('value_basket')) {
+                let basketMenu = document.querySelector('.basket_menu');
+                basketMenu.style.display = 'none';
+                openMainContent();
+            }
+        })
 })
